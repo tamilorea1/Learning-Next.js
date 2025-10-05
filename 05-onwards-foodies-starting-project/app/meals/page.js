d2e428c2ba167d5aead16c7ad6d1cc6a9ea8315d
tamilorea1 <tamiloreakinfemi@gmail.com>
2025-10-04 22:31:53 -0600

    learnt alot today. understood how to add some styling to next js. As well as using 'use client' when using hooks

--- 05-onwards-foodies-starting-project/app/meals/page.js
+++ 05-onwards-foodies-starting-project/app/meals/page.js
@@ -1,9 +1,29 @@
 import Link from "next/link";
-
+import classes from './page.module.css'
+import MealsGrid from "@/components/meals/meals-grid";
 export default function MealsPage(){
     return(
         <>
-            <h1>The Meals Page</h1>
+            <header className={classes.header}>
+
+                <h1>
+                    Delcious meals
+                </h1>
+
+                <p>Choose your fav recipe.</p>
+
+                <p className={classes.cta}>
+                    <Link href='/meals/share'>
+                        share your fav recipe
+                    </Link>
+                </p>
+
+            </header>
+
+            <main className={classes.main}>
+
+                <MealsGrid meals={[]}/>
+            </main>
             
            
         </>
